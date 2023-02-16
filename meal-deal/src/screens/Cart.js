aa8ef484dd1b65c8d4e773f8f30ecc0ff1726e2c
Samee <abdussamee602@gmail.com>
2023-02-16 04:29:11 -0500

    created basic cart template

--- meal-deal/src/screens/Cart.js
+++ meal-deal/src/screens/Cart.js
@@ -1,20 +1,55 @@
-import React, { useState } from "react";
-import { getCart, resetCart } from "../services/cartService";
+import React, { useState, useEffect } from "react";
+// import { getCart } from "../services/cartService";
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 import "../styles/auth.css";
+import axios from "axios";
 
 function Cart(props) {
-  const [cart, setCart] = useState({})
+  const [cart, setCart] = useState({});
+  const [items, setItems] = useState([]);
 
-  async function getCartData(){
-    let cartdata= await getCart(1);
-    setCart(cartdata);
-  }
-  getCartData()
+
+  useEffect(() => {
+      axios.get(`http://localhost:8080/cart/1`)
+      .then((respnse) => {
+        setCart(respnse.data);
+        setItems(respnse.data.items);
+      })
+  }, [])
+  
   console.log(cart);
-  resetCart(1);
+
+  // async function getCartData(){
+  //   let cartdata= await getCart(1);
+  //   setCart(cartdata);
+  // }
+  // getCartData()
+  //resetCart(1);
   return (
-    <h1>{cart.totalPrice}</h1>
+    <div className="container"><h2>Ordering from {cart.vendorName}</h2>
+    <br/>
+    <Table striped>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Price</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(item=>(
+          <tr>
+            <td>{item.itemName}</td>
+            <td>{item.price}</td>  
+            <td><Button variant="outline-danger" size="sm">-</Button> {item.quantity} <Button variant="outline-success">+</Button></td>
+          </tr>
+        ))}
+      </tbody>
+      <h4>Total: ${cart.totalPrice}</h4>
+    </Table>
+    </div>
   );
 }
 
