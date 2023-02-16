cea20bcfef82084037586582fb0a6f92e52ecec0
Hetvi2799 <hetvikhirsaria1999@gmail.com>
2023-02-15 22:42:16 -0500

    Updated Validations for login Page

--- meal-deal/src/screens/customerLogin.js
+++ meal-deal/src/screens/customerLogin.js
@@ -1,72 +1,57 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/login.css"
 import FormInput from "../common/formInput";
 
-import { clearToken } from "../services/clearJwt";
-import { loginCustomer as login } from "../services/customerService";
 
-import "../styles/auth.css";
-import { toast } from "react-toastify";
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-function CustomerLogin(props) {
-  const [account, setAccount] = useState({ email: "", password: "" });
-  let navigate = useNavigate();
-  useEffect(() => {
-    clearToken(props.updateToken);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const handleChange = ({ currentTarget: input }) => {
-    const acc = { ...account };
-    acc[input.name] = input.value;
-    setAccount(acc);
-  };
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    const result = await login(account, props.updateToken);
-    if (result === true) navigate("/customer");
-    else if (result) toast.error(result.data);
-  };
+  function handleLogin(event) {
+    event.preventDefault();
+    // Add code here to handle the login attempt
+  }
 
   return (
-    <React.Fragment>
-      <div className="card form-card shadow-lg">
-        <div className="card-body">
-          <form>
-            <h5 className="card-title mb-5">Customer Login</h5>
-            <FormInput
-              value={account.name}
-              type="email"
-              name="email"
-              onChange={handleChange}
+    <div className='card form-card shadow-lg'>
+      <form onSubmit={handleLogin}>
+      <h5 className="card-title mb-5">Customer Login</h5>
+      <FormInput
+              value={email}
+              type="text"
+              name="Email"
+              onChange={event => setEmail(event.target.value)}
             />
-            <FormInput
-              value={account.password}
+        <FormInput
+              value={password}
               type="password"
-              name="password"
-              onChange={handleChange}
+              name="Password"
+              onChange={event => setPassword(event.target.value)}
             />
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
-            </button>
-            <h6 className="message">
+        <button type="submit">Login</button>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+      </form>
+      <p><center>Don't have an account?</center></p>
+      <h6 className="message">
+        <center>
               Not a Customer?{" "}
               <Link className="pointer" to="/tiffin-vendor/login">
                 Tiffin Vendor
               </Link>
+              </center>
             </h6>
             <h6 className="message">
+              <center>
               Not Registered?{" "}
               <Link className="pointer" to="/customer/register">
                 Register
               </Link>
+              </center>
             </h6>
-          </form>
-        </div>
-      </div>
-    </React.Fragment>
+    </div>
   );
 }
 
-export default CustomerLogin;
+export default LoginPage;
--- meal-deal/src/screens/tiffinVendorLogin.js
+++ meal-deal/src/screens/tiffinVendorLogin.js
@@ -1,71 +1,57 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-
-import { loginTiffinVendor as login } from "../services/vendorService";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/login.css"
 import FormInput from "../common/formInput";
 
-import { clearToken } from "../services/clearJwt";
-import "../styles/auth.css";
 
-function TiffinVendorLogin(props) {
-  const [account, setAccount] = useState({ email: "", password: "" });
-  let navigate = useNavigate();
-  useEffect(() => {
-    clearToken(props.updateToken);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  const handleChange = ({ currentTarget: input }) => {
-    const acc = { ...account };
-    acc[input.name] = input.value;
-    setAccount(acc);
-  };
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    const result = await login(account, props.updateToken);
-    if (result === true) navigate("/tiffin-vendor");
-    else if (result) toast.error(result.data);
-  };
+  function handleLogin(event) {
+    event.preventDefault();
+    // Add code here to handle the login attempt
+  }
 
   return (
-    <React.Fragment>
-      <div className="card form-card shadow-lg">
-        <div className="card-body">
-          <form>
-            <h5 className="card-title mb-5">Tiffin Vendor Login</h5>
-            <FormInput
-              value={account.name}
-              type="email"
-              name="email"
-              onChange={handleChange}
+    <div className='card form-card shadow-lg'>
+      <form onSubmit={handleLogin}>
+      <h5 className="card-title mb-5">Vendor Login</h5>
+      <FormInput
+              value={email}
+              type="text"
+              name="Email"
+              onChange={event => setEmail(event.target.value)}
             />
-            <FormInput
-              value={account.password}
+        <FormInput
+              value={password}
               type="password"
-              name="password"
-              onChange={handleChange}
+              name="Password"
+              onChange={event => setPassword(event.target.value)}
             />
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
-            </button>
-            <h6 className="message">
+        <button type="submit">Login</button>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+      </form>
+      <p><center>Don't have an account?</center></p>
+      <h6 className="message">
+        <center>
               Not a Tiffin Vendor?{" "}
               <Link className="pointer" to="/customer/login">
                 Customer
               </Link>
+              </center>
             </h6>
             <h6 className="message">
+              <center>
               Not Registered?{" "}
               <Link className="pointer" to="/tiffin-vendor/register">
                 Register
               </Link>
+              </center>
             </h6>
-          </form>
-        </div>
-      </div>
-    </React.Fragment>
+    </div>
   );
 }
 
-export default TiffinVendorLogin;
+export default LoginPage;
