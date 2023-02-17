d54d379f48313ff4cdb6591f023d88ac163b1491
Samee <abdussamee602@gmail.com>
2023-02-16 21:19:37 -0500

    added edit quantity feature

--- meal-deal/src/screens/Cart.js
+++ meal-deal/src/screens/Cart.js
@@ -21,34 +21,60 @@ function Cart(props) {
   
   console.log(cart);
 
-  // async function getCartData(){
-  //   let cartdata= await getCart(1);
-  //   setCart(cartdata);
-  // }
-  // getCartData()
-  //resetCart(1);
+  const handleQuant= async (index, add)=>{
+    console.log(cart.items[index]);
+    let total= cart.totalPrice
+    if(add){
+      items[index].quantity+=1;
+      total+=items[index].price;
+    }
+    else{
+      items[index].quantity-=1;
+      total-=items[index].price;
+      if(items[index].quantity===0){
+        items.splice(index,1);
+      }
+    }
+    let newCartItems={...cart, items: items};
+    let newCart={...newCartItems, totalPrice: total};
+    setCart(newCart);
+    await fetch(`http://localhost:8080/cart/add/${cart.userId}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cart)
+    });
+  }
+
   return (
-    <div className="container"><h2>Ordering from {cart.vendorName}</h2>
-    <br/>
-    <Table striped>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>Price</th>
-          <th>Quantity</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(item=>(
-          <tr>
-            <td>{item.itemName}</td>
-            <td>{item.price}</td>  
-            <td><Button variant="outline-danger" size="sm">-</Button> {item.quantity} <Button variant="outline-success">+</Button></td>
-          </tr>
-        ))}
-      </tbody>
-      <h4>Total: ${cart.totalPrice}</h4>
-    </Table>
+    <div className="container">
+      {items.length>0?(
+      <><h2>Ordering from {cart.vendorName}</h2><br /><Table striped>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Price</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr>
+                <td>{item.itemName}</td>
+                <td>{item.price}</td>
+                <td>
+                  <Button variant="outline-danger" size="sm" onClick={() => handleQuant(index, false)}>-</Button>
+                  {item.quantity}
+                  <Button variant="outline-success" size="sm" onClick={() => handleQuant(index, true)}>+</Button></td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <h3>Total: ${cart.totalPrice}</h3></>
+      ):(
+        <h2>Cart Empty...</h2>
+      )}
     </div>
   );
 }
