cf1f49eb11dc34cb0e876c77aff319a41c45ed43
abdu26399 <ahmedunofficial2603@gmail.com>
2023-03-31 02:54:07 -0400

    Fixed bugs in date pickers

--- meal-deal/src/screens/tiffinVendorDetails.js
+++ meal-deal/src/screens/tiffinVendorDetails.js
@@ -23,6 +23,8 @@ function TiffinVendorDetails(props) {
   const [isSelected, setIsSelected] = useState(false);
   const [index, setIndex] = useState(0);
   const [opacity, setOpacity] = useState(0.1);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
@@ -37,9 +39,42 @@ function TiffinVendorDetails(props) {
     setEndDate(date);
   };
 
+  const isWeekday = (date) => {
+    const day = date.getDay();
+    // Return true if the day is Monday (1) to Friday (5)
+    return day >= 1 && day <= 5;
+  };
+  
+  const getNextMonday = () => {
+    const today = new Date();
+    const day = today.getDay();
+    const daysUntilMonday = day === 0 ? 1 : 8 - day;
+    const nextMonday = new Date(today);
+    nextMonday.setDate(today.getDate() + daysUntilMonday);
+    return nextMonday;
+  };
+
   const isWeekend = (date) => {
     const day = date.getDay();
     return day === 0 || day === 6;
+  };
+
+  const getNextWeekend = () => {
+    const today = new Date();
+    const day = today.getDay();
+  // If today is a Saturday or Sunday, return today
+  if (day === 6 || day === 0) {
+    return today;
+  }
+  
+    // Otherwise, find the next Saturday or Sunday
+    const nextSaturday = new Date(today);
+    nextSaturday.setDate(today.getDate() + ((6 - day + 7) % 7));
+    const nextSunday = new Date(today);
+    nextSunday.setDate(today.getDate() + ((7 - day + 7) % 7));
+  
+    // Return the closest weekend day
+    return nextSunday < nextSaturday ? nextSunday : nextSaturday;
   };
 
   const renderDatePickers = () => {
@@ -56,6 +91,7 @@ function TiffinVendorDetails(props) {
                 onChange={handleStartDateChange}
                 dateFormat="dd/MM/yyyy"
                 filterDate={isWeekend}
+                minDate = {getNextWeekend()}
                 placeholderText="Select start date"
               />
             </div>
@@ -66,12 +102,41 @@ function TiffinVendorDetails(props) {
                 onChange={handleEndDateChange}
                 dateFormat="dd/MM/yyyy"
                 filterDate={isWeekend}
-                minDate={startDate}
+                minDate={getNextWeekend()}
                 placeholderText="Select end date"
               />
             </div>
           </div>
         );
+        case "Weekdays":
+          const today = new Date();
+          const startDate = isWeekend(today) ? getNextMonday() : today;
+          return (
+            <div className="row">
+              <div className="col-md-6 date--range m-0">
+                <label htmlFor="startDate">Start Date: </label>
+                <DatePicker
+                  selected={startDate}
+                  onChange={handleStartDateChange}
+                  dateFormat="dd/MM/yyyy"
+                  filterDate={isWeekday}
+                  minDate={today}
+                  placeholderText="Select start date"
+                />
+              </div>
+              <div className="col-md-6 date--range m-0">
+                <label htmlFor="endDate">End Date: </label>
+                <DatePicker
+                  selected={endDate}
+                  onChange={handleEndDateChange}
+                  dateFormat="dd/MM/yyyy"
+                  filterDate={isWeekday}
+                  minDate={startDate}
+                  placeholderText="Select end date"
+                />
+              </div>
+            </div>
+          );
       default:
         return (
           <div className="row">
@@ -112,36 +177,37 @@ function TiffinVendorDetails(props) {
     const getshow = e.target.value;
     setShowHide(getshow);
     setShowPrice(prices[getshow]);
-    setIsSelected = true;
+    setIsSelected(true);
   };
 
-  // var dataStore ={
+  // var dataStore = {
   //   userId: 1,
-  //   vendorName : data.vendorName,
-  //   items:
-  //   [
-  //   {
-  //     subscription : showHide,
-  //     price: showPrice,
-  //     quantity : quantity,
+  //   vendorId: data.id,
+  //   vendorName: data.vendorName,
+  //   startDate: startDate.toISOString().substring(0, 10),
+  //   endDate: endDate ? endDate.toISOString().substring(0, 10) : null,
+  //   items: [
+  //     {
+  //       subscription: showHide,
+  //       price: showPrice,
+  //       quantity: quantity,
+  //     },
+  //   ],
+  // };
 
-  //   }
-  //   ]
-  //   }
+  // const images = [
+  //   "https://picsum.photos/id/1015/800/600",
+  //   "https://picsum.photos/id/1016/800/600",
+  //   "https://picsum.photos/id/1018/800/600",
+  // ];
 
-  const images = [
-    "https://picsum.photos/id/1015/800/600",
-    "https://picsum.photos/id/1016/800/600",
-    "https://picsum.photos/id/1018/800/600",
-  ];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % images.length);
-      setOpacity(0.1);
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  //     setOpacity(0.1);
+  //   }, 3000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   useEffect(() => {
     const url = `http://mealdeal.herokuapp.com/vendor/${params.id}`;
@@ -166,6 +232,20 @@ function TiffinVendorDetails(props) {
   }, []);
 
   async function handleClick() {
+
+    if (
+      (showHide === "Week" || showHide === "Weekdays" || showHide === "Fortnight" || showHide === "Month") &&
+      (!startDate || !endDate)
+    ) {
+      alert("Please select both start and end dates.");
+      return;
+    }
+
+    if (quantity < 1) {
+      alert("Please enter a quantity of at least 1.");
+      return;
+    }
+
     var dataStore = {
       userId: 1,
       vendorId: data.id,
@@ -175,12 +255,15 @@ function TiffinVendorDetails(props) {
           subscription: showHide,
           price: showPrice,
           quantity: quantity,
+          startDate: startDate.toISOString().substring(0, 10),
+          endDate: endDate.toISOString().substring(0, 10),
         },
       ],
     };
-
+    console.log(typeof(dataStore.endDate));
+    console.log(dataStore);
     // Send data to the backend via POST
-    await fetch(`http://mealdeal.herokuapp.com/cart/add/${userId}`, {
+    const response = await fetch(`http://mealdeal.herokuapp.com/cart/add/${userId}`, {
       // Enter your IP address here
 
       method: "PUT",
@@ -188,14 +271,36 @@ function TiffinVendorDetails(props) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(dataStore),
+      
     });
+
+    if (response.ok) {
+      setAlertMessage("Added to cart successfully!");
+      setShowAlert(true); // Show the alert
+      setTimeout(() => {
+        setShowAlert(false); // Hide the alert after 4 seconds
+      }, 4000);
+    } else {
+      setAlertMessage("Failed to add to cart.");
+      setShowAlert(true); // Show the alert
+      setTimeout(() => {
+        setShowAlert(false); // Hide the alert after 4 seconds
+      }, 4000);
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
   }
 
   return (
     <div className="tiffin-vendor-detail mt-1">
       <section className="section-content padding-y bg">
         <div className="container">
+        {showAlert && (
+            <div className="alert alert-success" role="alert">
+              {alertMessage}
+            </div>
+          )}
           <h3 className="title">{data.vendorName}</h3>
+          <div className="vendor-address">{data.address}</div>
           <div className="row">
             <div className="col-md-6">
               <div className="vendor-subscription--card">
@@ -399,23 +504,20 @@ function TiffinVendorDetails(props) {
                       ))}
                   </ul> */}
 
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th scope="col">Item</th>
-                        <th scope="col">Price</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {menu &&
-                        Object.keys(menu).map((key) => (
-                          <tr key={key} scope="row">
-                            <td>{key}</td>
-                            <td>{menu[key]}</td>
-                          </tr>
-                        ))}
-                    </tbody>
-                  </table>
+<table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Item</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+        <pre>{data.menu}</pre>
+      </td>
+    </tr>
+  </tbody>
+</table>
                 </div>
               </div>
             </div>
@@ -425,9 +527,11 @@ function TiffinVendorDetails(props) {
                   <img
                     className="vendor--card-img img-responsive center-block d-block mx-auto"
                     src={data.image}
-                  />
+                  />  
                 </div>
               </div>
+              <div>
+  </div>
             </div>
           </div>
         </div>
